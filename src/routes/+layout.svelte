967e14d880fc1a512bde9d0e1c4da5e5d7d1b32d
Mohsen Heidari <chapanh@iran.ir>
2024-01-28 19:24:10 +0330

    ToDo_List: 01

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<section class="bg-slate-900 h-screen">
+	<div class="relative justify-center my-0 mx-auto p-5 w-[360px] sm:w-[640px] md:w-[768px]">
+		<main>
+			<slot />
+		</main>
+	</div>
+</section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	let ToDoList: any = []; // Array Of ToDos
+	let textInput: string = '';
+
+	function addToDo(): void {
+		ToDoList = [...ToDoList, { content: textInput, editing: false, checked: false }];
+
+		textInput = '';
+	}
+
+	function setEdeting(counter: number, isEdeting: boolean) {
+		ToDoList[counter].editing = isEdeting; // True Or False
+	}
+
+	function deleteToDo(counter: number) {
+		ToDoList.splice(counter, 1);
+		ToDoList = ToDoList;
+	}
+</script>
+
+<svelte:head>
+	<title>To-Do List</title>
+</svelte:head>
+
+<h1 class="text-center text-teal-50 text-5xl font-sans font-black capitalize mt-10 mb-20">
+	ToDo list
+</h1>
+
+<p class="text-lg text-slate-400 font-sans font-normal capitalize mb-4">enter your to-do here</p>
+<div class="flex justify-center gap-2 mx-2 mb-7">
+	<input
+		placeholder="Add ToDo"
+		class="flex flex-auto bg-white/5 border-teal50 p-2 border-white/20 border-2 font-sans font-normal rounded-md text-white"
+		type="text"
+		bind:value={textInput}
+	/>
+	<button
+		class="bg-cyan-600 rounded-md text-white font-sans font-normal capitalize h-11 w-auto p-2"
+		on:click={addToDo}>add</button
+	>
+</div>
+
+{#each ToDoList as ToDo, counter}
+	<section class="flex items-baseline gap-2 w-auto my-0 mx-auto mb-2">
+		{#if ToDo.editing}
+			<input
+				class="flex flex-auto bg-white/5 border-teal50 p-2 border-white/20 border-2 font-sans font-normal h-11 rounded-md text-white"
+				type="text"
+				bind:value={ToDo.content}
+			/>
+		{:else}
+			<input
+				class="relative float-left mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none
+        before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-['']
+        checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem]
+        checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-['']
+        hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12]
+        focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem]
+        focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]
+        checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem]checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem]
+        checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent
+        dark:border-white/20 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+				type="checkbox"
+				bind:checked={ToDo.checked}
+			/>
+			<h3 class="flex-grow font-sans font-normal text-teal-50 capitalize">{ToDo.content}</h3>
+		{/if}
+
+		<!-- Buttons Start -->
+		<div class="flex gap-1">
+			{#if ToDo.editing}
+				<button
+					class="bg-cyan-600 rounded-md text-white font-sans font-normal capitalize h-11 w-auto p-1"
+					on:click={() => setEdeting(counter, false)}>save</button
+				>
+			{:else}
+				<button
+					class="bg-cyan-600 rounded-md text-white font-sans font-normal capitalize h-11 w-auto p-1"
+					on:click={() => setEdeting(counter, true)}>edit</button
+				>
+			{/if}
+			<button
+				class="bg-cyan-600 rounded-md text-white font-sans font-normal capitalize h-11 w-auto p-1"
+				on:click={() => deleteToDo(counter)}>delete</button
+			>
+		</div>
+		<!-- Buttons End -->
+	</section>
+{/each}
